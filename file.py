4d2cfda0a70fabb1fdb5315eafe29075ff0388a9
repl.it user <replituser@example.com>
2021-01-05 08:21:21 +0000

    esercizio  quasi finito

--- file.py
+++ file.py
@@ -1,4 +1,15 @@
-my_file = open("shampoo_sales.csv", "r")
-print(my_file.readline())
-print(my_file.readline())
-my_file.close()
+# Inizializzo una lista vuota per salvare i valori
+values = []
+# Apro e leggo il file, linea per linea
+my_file = open("shampoo_sales.txt", "r")
+for line in my_file:
+ # Faccio lo split di ogni riga sulla virgola
+    elements = line.split(',')
+ # Se NON sto processando l’intestazione...
+    if elements[0] != 'Date':
+
+        date = elements[0]
+        value = elements[1]
+    values.append(float(value))
+
+print(values)
