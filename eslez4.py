b5d971aa75c9c1abcab45b2c4bd49725c2ba048f
repl.it user <replituser@example.com>
2021-01-09 14:37:31 +0000

    fatto

--- eslez4.py
+++ eslez4.py
@@ -1,5 +1,6 @@
 print("Non e` il risultato che volevi")
 #non e` il risultato del ez 4
-#quello lo trovi su file.py
+#quello lo trovi su es5.py
 #tutte le modifiche andranno li
-
+#guarda la storia per vedere l  es 4
+#oppure la lezione
