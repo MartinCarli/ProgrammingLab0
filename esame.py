9d740125b429a45cca56754a200ffdef4e8526f6
repl.it user <replituser@example.com>
2021-02-03 07:43:43 +0000

    fatto

--- esame.py
+++ esame.py
@@ -1,36 +1,62 @@
-class ExamException(Exception):     #per alzare le eccezioni
+#prog del prof
+#esame1
+
+class ExamException(Exception):
     pass
 
-class MovingAverage:
-    def __init__(self,lunghezza):
-        self.lunghezza = lunghezza
-
-    def compute(self,valori):
-        lista_valori=[]
+class MovingAverage():
+    
+    def __init__(self, window):
         
-        if not isinstance(valori,list):
-            raise ExamException('Errore i valori non sono in una lista')
+        # Controllo che la lunghezza della finestra si di tipo intero
+        if not isinstance(window, int):
+            raise ExamException('Invalid type for window, only int supported. Got "{}"'.format(type(window)))
 
-        nval == len(valori)
+        # Controllo che la lunghezza della finestra non sia uguale a zero o negativa.
+        # nota bene che una finestra di lunghezza uno e' accettata!
+        if window < 1:
+            raise ExamException('Negative or zero window value provided')
+        
+        # Salvo la lunghezza della finestra internamente
+        self.window = window
+    
+    def compute(self, data):
 
-        if mialung > nval:
-            raise ExamException('Errore la lunghezza deve essere minore o ugale al numero dei valori')
-        if valori is None:
-            raise ExamException('Errore la lista e vuota')
+        # Controllo che i valori siano in una lista
+        if not isinstance(data, list):
+            raise ExamException('Invalid type for data, only list supported. Got "{}"'.format(type(data)))
 
-        valore=0
-        mialung == lunghezza
-       
-
-        for i in valori:
-            valore=0
-            k==i
-            if (i+mialung>=nval):
+        # Controllo che la lista sia abbastanza lunga
+        if len(data) < self.window:
+            raise ExamException('Not enough data to compute a moving average of "{}" steps'.format(self.window))
+        
+        # Controllo che i valori della lista siano di tipo float o numerico.
+        # Questo controllo non era veramente richiesto, e puo' alle volte
+        # risultare lento perchè aggiungo un ciclo su tutti gli elementi della lista.
+        # Tuttavia dava punti extra nella valutazione, per premiare chi ci ha pensato
+        # e si e' posto il problema a prescindere dalle performance (che non sono
+        # argomento di quetso corso).
+        
+        for item in data:
+            if not (isinstance(item, int) or  isinstance(item, float)):
+                raise ExamException('Got non-numeric item in the list data: "{}"'.format(item))
+        
+        # Ok, ora calcolo la media mobile ciclando su tutti gli elmenti della lista. Per la natura
+        # del problema, non posso essere del tutto pythonico ed usar eper esempio il costrutto 
+        # "for item in list", ma devo usare un' indice di supporto.
+        
+        averages = []
+        for i in range(len(data)+1):
+            
+            # Se non ho ho abbastanza valori su cui applicarla, 
+            # continuo andando al prossimo giro
+            if i < self.window:
                 continue
-            while(k is not mialung):
-                valore+= valori[k]
-                k+=1
-            valore== valore/mialung
-            lista_valori.append(valore)
+            else:
+                # Forma contratta che usa la "sum" built-in. Si poteva implementare
+                # questa parte in molti altri modi, piu' o meno contratti.
+                averages.append(sum(data[(i-self.window):i])/self.window)
+                
+        return averages
 
-        return lista_valori
+
